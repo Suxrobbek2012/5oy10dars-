ce38e36d4fabfc3d7582a47ade9a2aa43f8a4a61
Suxrobbek2012 <suxrobbek714@gmail.com>
2025-03-01 10:30:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,141 @@
+
+// const username = prompt ("ismingizni kiriting")
+    
+// const h1 = document.createElement ("h1")
+
+// h1 .textContent = username;
+// document.body.append(h1)
+// const rang = prompt("ismingizni kriting");
+// rang.stayle.backgroundCollor = ("red")
+// document.body.appendChild(rang)
+// const h1 = document.createElement ("h1")
+// h1 .textContent = rang;
+// document.body.append(h1)
+// rang.stayle.backgroundCollor = "red"
+
+
+// let textContent = document.getElementById("dynamicStyles");
+// textContent.innerHTML += "#myDiv { font-size: 25px; background-color: green; }";
+
+
+
+
+const ism = prompt("ismingizni kiriting:");
+
+
+const ismrang = prompt("So'zingizning rangini kiriting(red):");
+
+
+const outputDiv = document.getElementById("idism");
+outputDiv.innerText = ism;
+outputDiv.style.color = ismrang;
+// const username = prompt ("ismingizni kiriting")
+// const username1 = prompt ("familiyangizni kiriting")
+// const username3 = prompt ("yosh  kiriting")      
+// const h1 = document.createElement ("h1")
+// const h2 = document.createElement ("h2")
+// const h3 = document.createElement ("h3")
+// h1 .textContent = username;
+// document.body.append(h1)
+// h2 .textContent = username1;
+// document.body.append(h2)
+// h3 .textContent = username3;
+// document.body.append(h3)
+// const rang = document.createElement["collor"];
+// rang.stayle.backgroundCollor = "red"
+// const username = prompt ("ismingizni kiriting")
+// const username1 = prompt ("familiyangizni kiriting")
+// const username3 = prompt ("yosh  kiriting")      
+// const h1 = document.createElement ("h1")
+// const h2 = document.createElement ("h2")
+// const h3 = document.createElement ("h3")
+// h1 .textContent = username;
+// document.body.append(h1)
+// h2 .textContent = username1;
+// document.body.append(h2)
+// h3 .textContent = username3;
+// document.body.append(h3)
+
+
+// const num1 = +prompt("birinchi sonni kriting");
+// const num2 = +prompt("ikkinchi sonni kriting");
+// const num3 = +prompt("operator tanglang \n 1) +\n  2) -\n  3) *\n 4) /");
+// const h1 = document.createElement ("h1")
+// const h2 = document.createElement ("h2")
+// const h3 = document.createElement ("h3")
+// h1 .textContent = num1;
+// document.body.append(h1. $num1 + $num2)
+// h2 .textContent = num2;
+// document.body.append(h2. $num1 + $num2)
+// h3 .textContent = num3;
+// document.body.append(h3. $num1 + $num2)
+
+
+// const ism = prompt("ismingizni kriting")
+// const h3 = document.createElement ("h3")
+// h3 .mol = ism;
+// document.body.append(h3.textContent) 
+// console.log(ism);
+
+
+
+
+// const num1 = +prompt("Birinchi sonni kiriting");
+// const num2 = +prompt("Ikkinchi sonni kiriting");
+// const operator = +prompt("Operator tanlang \n 1) +\n  2) -\n  3) *\n 4) /");
+
+// let result;
+
+// switch (operator) {
+//     case 1:
+//         result = num1 + num2;
+//         break;
+//     case 2:
+//         result = num1 - num2;
+//         break;
+//     case 3:
+//         result = num1 * num2;
+//         break;
+//     case 4:
+//         if (num2 !== 0) {
+//             result = num1 / num2;
+//         } else {
+//             result = "Nolga bo'linish mumkin emas!";
+//         }
+//         break;
+//     default:
+//         result = "Noto'g'ri operator tanlandi.";
+// }
+
+// alert("Natija: " + result);
+// document.getElementById("calculateBtn").addEventListener("click", function() {
+//     const num1 = parseFloat(document.getElementById("num1").value);
+//     const num2 = parseFloat(document.getElementById("num2").value);
+//     const operator = parseInt(document.getElementById("operator").value);
+
+//     let result;
+
+//     switch (operator) {
+//         case 1:
+//             result = num1 + num2;
+//             break;
+//         case 2:
+//             result = num1 - num2;
+//             break;
+//         case 3:
+//             result = num1 * num2;
+//             break;
+//         case 4:
+//             if (num2 !== 0) {
+//                 result = num1 / num2;
+//             } else {
+//                 result = "Nolga bo'linish mumkin emas!";
+//             }
+//             break;
+//         default:
+//             result = "Noto'g'ri operator tanlandi.";
+//     }
+
+//     // Natijani DOM orqali chiqarish
+//     document.getElementById("result").innerText = "Natija: " + result;
+// });
